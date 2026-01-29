ad35f3ac90369b0c8c3295bb4e0cfb493c0c4493
Adriaan Jansen <adriaanj123@gmail.com>
2026-01-28 19:41:58 -0500

    add other two phases and fix duty cycle issue

--- controller.ino
+++ controller.ino
@@ -1,16 +1,21 @@
-#include <math.h>
 #include "driver/mcpwm_prelude.h"
 #include "driver/gpio.h"
 
-#define LUT_SIZE 256  // Sine table size
+#define LUT_SIZE 360  // Sine table size
 
-#define P1H 12  // GPIO 12
-#define P1L 14  // GPIO 14
+#define P1H 12 // GPIO 12
+#define P1L 14 // GPIO 14
+#define P2H 27 // GPIO 27
+#define P2L 26 // GPIO 26
+#define P3H 25 // GPIO 25
+#define P3L 33 // GPIO 33
 
 #define DEAD_TIME_TICKS 10
+#define MCPWM_HZ 8000000
+#define PWM_MAX 1000
 
 // sine table
-uint16_t sin_table[LUT_SIZE];
+uint32_t sin_table[LUT_SIZE];
 
 // phase index
 volatile int phase_index = 0;
@@ -19,27 +24,48 @@ volatile int phase_index = 0;
 mcpwm_timer_handle_t timer = NULL;
 
 // comparators
-mcpwm_cmpr_handle_t comparators[1];
+mcpwm_cmpr_handle_t comparators[3];
 
-// Init MCPWM timer
-void mcpwm_gpio_init(void) {
+void generate_sine_table()
+{
+    for (uint32_t i = 0; i < LUT_SIZE; i++) {
+        sin_table[i] = (uint32_t)((sin((double)i * PI / 180) * (PWM_MAX / 2)) + (PWM_MAX / 2));
+    }
+}
+
+static bool IRAM_ATTR on_timer(mcpwm_timer_handle_t t, const mcpwm_timer_event_data_t *edata, void *user_data)
+{
+  phase_index = (phase_index + 1) % LUT_SIZE;
+
+  uint16_t SINE_A_PWM = sin_table[phase_index];
+  uint16_t SINE_B_PWM = sin_table[(phase_index + 120) % LUT_SIZE];
+  uint16_t SINE_C_PWM = sin_table[(phase_index + 240) % LUT_SIZE];
+
+  mcpwm_comparator_set_compare_value(comparators[0], SINE_A_PWM);
+  mcpwm_comparator_set_compare_value(comparators[1], SINE_B_PWM);
+  mcpwm_comparator_set_compare_value(comparators[2], SINE_C_PWM);
+
+  return false;
+}
+
+void mcpwm_gpio_init(void)
+{
   // Init MCPWM timer
   mcpwm_timer_config_t timer_config = {
     .group_id = 0,
     .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
-    .resolution_hz = 1000000,
+    .resolution_hz = MCPWM_HZ,
     .count_mode = MCPWM_TIMER_COUNT_MODE_UP_DOWN,
-    .period_ticks = 500,
+    .period_ticks = 2000,
   };
   ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &timer));
 
   // Create MCPWM operators
-  int high_pins[1] = { P1H };
-  int low_pins[1] = { P1L };
-  mcpwm_oper_handle_t operators[1];
+  int high_pins[3] = { P1H, P2H, P3H };
+  int low_pins[3] = { P1L, P2L, P3L };
+  mcpwm_oper_handle_t operators[3];
 
-  // would normally loop 3 times but just testing with 1 phase for now
-  for (int i = 0; i < 1; i++) {
+  for (int i = 0; i < 3; i++) {
     // Operator, responsible for generating PWM waveforms
     mcpwm_operator_config_t operator_config = { .group_id = 0 };
     mcpwm_new_operator(&operator_config, &operators[i]);
@@ -50,6 +76,7 @@ void mcpwm_gpio_init(void) {
     // Comparator, handles duty cycle
     mcpwm_comparator_config_t compare_config = {};
     compare_config.flags.update_cmp_on_tez = true;
+    compare_config.flags.update_cmp_on_tep = true,
     mcpwm_new_comparator(operators[i], &compare_config, &comparators[i]);
 
     // Connect PWM to physical pins
@@ -59,57 +86,55 @@ void mcpwm_gpio_init(void) {
     generator_config.gen_gpio_num = low_pins[i];
     mcpwm_new_generator(operators[i], &generator_config, &gen_l);
 
-    // high at 0 low at match
-    mcpwm_generator_set_action_on_timer_event(gen_h, MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH));
-    mcpwm_generator_set_action_on_compare_event(gen_h, MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparators[i], MCPWM_GEN_ACTION_LOW));
+    // Set HIGH when timer is 0 and starting to count UP
+    mcpwm_generator_set_action_on_timer_event(gen_h, 
+        MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH));
 
-    mcpwm_generator_set_action_on_timer_event(gen_l, MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH));
-    mcpwm_generator_set_action_on_compare_event(gen_l, MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparators[i], MCPWM_GEN_ACTION_LOW));
+    // Set LOW when timer hits match while counting UP
+    mcpwm_generator_set_action_on_compare_event(gen_h, 
+        MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparators[i], MCPWM_GEN_ACTION_LOW));
 
-    // dead time
+    // Set HIGH when timer hits match while counting DOWN
+    mcpwm_generator_set_action_on_compare_event(gen_h, 
+        MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_DOWN, comparators[i], MCPWM_GEN_ACTION_HIGH));
+
+    // Set LOW when timer is 0 after counting DOWN
+    mcpwm_generator_set_action_on_timer_event(gen_h, 
+        MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_DOWN, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_LOW));
+
+
+    // Dead time
     mcpwm_dead_time_config_t dt_config = {};
     dt_config.posedge_delay_ticks = DEAD_TIME_TICKS;
 
-    // apply config to highside
+    // Apply config to highside
     mcpwm_generator_set_dead_time(gen_h, gen_h, &dt_config);
 
-    // apply config to lowside
+    // Apply config to lowside
     dt_config.negedge_delay_ticks = DEAD_TIME_TICKS;
-    dt_config.flags.invert_output = true; 
+    dt_config.flags.invert_output = true;
 
-    // make low side inverse of high side and apply config
+    // Make low side inverse of high side and apply config
     mcpwm_generator_set_dead_time(gen_h, gen_l, &dt_config);
+
+    mcpwm_timer_event_callbacks_t cbs = {
+      .on_empty = on_timer,   // TEZ (count==0)
+    };
+    mcpwm_timer_register_event_callbacks(timer, &cbs, NULL);
   }
 
   mcpwm_timer_enable(timer);
   mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP);
 }
 
-void generate_sine_table(uint32_t period) {
-  for (int i = 0; i < LUT_SIZE; i++) {
-    // scale sin wave (-1 to 1) to (0 to period)
-    sin_table[i] = (uint32_t)(((sin(2.0 * M_PI * i / LUT_SIZE) + 1.0) / 2.0) * period);
-  }
-}
 
-hw_timer_t *motor_timer = NULL;
-void ARDUINO_ISR_ATTR on_timer() {
-  phase_index = (phase_index + 1) % LUT_SIZE;
-}
-
-void setup() {
+void setup()
+{
+  generate_sine_table();
   mcpwm_gpio_init();
-  generate_sine_table(500);
-
-  motor_timer = timerBegin(10000);
-  timerAttachInterrupt(motor_timer, &on_timer);
-  timerAlarm(motor_timer, 200, true, 0);
 }
 
-void loop() {
-  uint32_t valA = sin_table[phase_index];
-
-  mcpwm_comparator_set_compare_value(comparators[0], valA);
-
-  delay(1);
+void loop()
+{
+  // do nothing
 }
